97cd2b6bf18b3e3f858b736cb5d839c495cf4039
Floyda <floyda@163.com>
2019-09-02 15:54:03 +0800

    modify path lib

--- resource.py
+++ resource.py
@@ -11,9 +11,13 @@ def get_cache_file_path(name):
     return os.path.join(sublime.cache_path(), "User", "%s.cache" % name)
 
 
+def get_user_path():
+    return os.path.join(sublime.packages_path(), "User")
+
+
 def get_user_setting_path(name):
-    return os.path.join(sublime.packages_path(), "User", "%s.sublime-settings" % name)
+    return os.path.join(get_user_path(), "%s.sublime-settings" % name)
 
 
 def get_user_project_path(name):
-    return os.path.join(sublime.packages_path(), "User", "%s.sublime-project" % name)
+    return os.path.join(get_user_path(), "%s.sublime-project" % name)
